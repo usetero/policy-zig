e5e1c11b277e6691fc5dec539a90fb354214a77d
Jacob Aronoff <jaronoff97@users.noreply.github.com>
2026-02-12 12:46:43 -0500

    fix: expose otel protos (#11)

--- src/proto/root.zig
+++ src/proto/root.zig
@@ -1,3 +1,7 @@
 pub const policy = @import("tero/policy/v1.pb.zig");
 pub const common = @import("opentelemetry/proto/common/v1.pb.zig");
+pub const logs = @import("opentelemetry/proto/logs/v1.pb.zig");
+pub const metrics = @import("opentelemetry/proto/metrics/v1.pb.zig");
+pub const trace = @import("opentelemetry/proto/trace/v1.pb.zig");
+pub const resource = @import("opentelemetry/proto/resource/v1.pb.zig");
 pub const protobuf = @import("protobuf");
